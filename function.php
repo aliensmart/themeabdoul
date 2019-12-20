a4dfa0c13b56e3250e0d07c092b6b3c65e0aa7b0
aliensmart <kaoua17@gmail.com>
2019-12-20 15:04:38 -0500

    first theme dev

---
+++ function.php
@@ -0,0 +1,39 @@
+<?php
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'status',
+    'video', 'audio', 'chat']);
+    add_theme_support('custom-background');
+    add_theme_support('custom-Header');
+    add_theme_support('custom-logo');
+    add_theme_support('custom-feed-links');
+    add_theme_support('html5');
+    add_theme_support('customize-selective-refresh-widgets');
+    add_theme_support('starter-content');
+//Add css
+
+    function themeabdoul_enqueue_styles(){
+
+        wp_enqueue_style('main-css', get_stylesheet_directory_uri().'/style.css', [], time(), 'all' );
+    }
+    add_action( 'wp_enqueue_scripts', 'themeabdoul_enqueue_style')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
